db36bf0ee4d9a8347bbc60ad04ec76e0afa0b5bc
alternant <alternant@alternant-debian>
2016-11-25 09:43:23 +0100

    Barre de recherche fonctionnelle

--- scripts/rechercher.js
+++ scripts/rechercher.js
@@ -6,15 +6,28 @@ function rechercher_in_page(str) {
       var found = 0;
       if (articles[i].hasChildNodes()) {
           var childs = articles[i].childNodes;
-          for(j = 0; childs.length; j++) {
-            if(childs[j].innerHTML != null && childs[j].innerHTML.indexOf(str) != -1) {
+          console.log(childs);
+          console.log(childs[0].innerHTML);
+          for(j = 0; j < childs.length; j++) {
+            console.log(j);
+            var element = childs[j];
+            console.log(element);
+            if(element.innerHTML != null && element.innerHTML.indexOf(str) != -1) {
               found = 1;
             }
           }
       }
       if(found == 0) {
         articles[i].style.display = "none";
+      } else {
+        articles[i].style.display = "inline-block";
       }
     }
+  } else {
+    var articles = document.getElementsByTagName("article");
+    for(i = 0; i < articles.length; i++) {
+      articles[i].style.display = "inline-block";
+    }
   }
+  return false;
 }
